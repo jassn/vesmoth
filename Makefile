1626477695dd7f1deac6d59f26666b4990b18f7e
jason <jsnsgt@gmail.com>
2019-04-07 11:30:19 +0800

    initial commit of w-0.4.8

---
+++ Makefile
@@ -0,0 +1,30 @@
+## $Id: Makefile 34 2003-09-19 10:56:52Z zas $
+
+CC=g++
+ifndef CXXFLAGS
+CXXFLAGS=-g -O2 -Wall
+endif
+SDL_CFLAGS=`sdl-config --cflags` `freetype-config --cflags`
+SDL_LIBS=`sdl-config --libs` `freetype-config --libs` -lSDL_mixer -lSDL_ttf -lSDL_image
+LIBS=${SDL_LIBS} -lstdc++
+INCLUDES=-I. -Isrc -Isrc/tools -Isrc/widgets
+OBJS=src/actions.o src/ai.o src/ai_attack.o src/ai_move.o src/config.o src/dialogs.o src/display.o src/filesystem.o src/font.o src/game.o src/game_config.o src/game_events.o src/gamestatus.o src/hotkeys.o src/intro.o src/key.o src/language.o src/log.o src/map.o src/menu.o src/multiplayer.o src/pathfind.o src/playlevel.o src/playturn.o src/preferences.o src/replay.o src/sdl_utils.o src/sound.o src/team.o src/terrain.o src/unit.o src/unit_types.o src/video.o src/widgets/button.o src/widgets/slider.o src/widgets/textbox.o
+
+MAKE_TRANS_OBJS=src/tools/make_translation.o src/config.o src/filesystem.o src/log.o
+MERGE_TRANS_OBJS=src/tools/merge_translations.o src/config.o src/filesystem.o src/log.o
+
+wesnoth: $(OBJS)
+	${CC} ${CXXFLAGS} ${INCLUDES} ${SDL_CFLAGS} -o $@ ${OBJS} ${LIBS}
+
+make_translation: $(MAKE_TRANS_OBJS)
+	${CC} ${CXXFLAGS} ${INCLUDES} -o $@ ${MAKE_TRANS_OBJS} ${LIBS}
+
+merge_translations: $(MERGE_TRANS_OBJS)
+	${CC} ${CXXFLAGS} ${INCLUDES} -o $@ ${MERGE_TRANS_OBJS} ${LIBS}
+
+.cpp.o:
+	${CC} ${CXXFLAGS} ${INCLUDES} ${SDL_CFLAGS} -c $< -o $*.o
+
+.PHONY: clean
+clean:
+	-rm -f ${OBJS} ${MAKE_TRANS_OBJS} ${MERGE_TRANS_OBJS} wesnoth make_translation merge_translations
---
+++ src/video.hpp
@@ -0,0 +1,71 @@
+/* $Id: video.hpp 97 2003-09-26 11:52:42Z Sirp $ */
+/*
+   Copyright (C) 2003 by David White <davidnwhite@optusnet.com.au>
+   Part of the Battle for Wesnoth Project http://wesnoth.whitevine.net
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+#ifndef VIDEO_HPP_INCLUDED
+#define VIDEO_HPP_INCLUDED
+
+#include "SDL.h"
+
+//possible flags when setting video modes
+#define FULL_SCREEN SDL_FULLSCREEN
+#define VIDEO_MEMORY SDL_HWSURFACE
+#define SYSTEM_MEMORY SDL_SWSURFACE
+
+class CVideo {
+     public:
+	CVideo(const char* text);
+	CVideo( int x, int y, int bits_per_pixel, int flags, const char* text );
+	~CVideo();
+
+	int modePossible( int x, int y, int bits_per_pixel, int flags );
+	int setMode( int x, int y, int bits_per_pixel, int flags );
+
+	//functions to get the dimensions of the current video-mode
+	int getx() const;
+	int gety() const;
+	int getBitsPerPixel();
+	int getBytesPerPixel();
+	int getRedMask();
+	int getGreenMask();
+	int getBlueMask();
+
+	//functions to access the screen
+	void lock();
+	void unlock();
+	int mustLock();
+
+	void flip();
+
+	SDL_Surface* getSurface( void );
+
+	bool isFullScreen() const;
+
+	struct error {};
+
+	struct quit {};
+
+     private:
+
+	SDL_Surface* frameBuffer;
+	char text_[256*8];
+};
+
+void allow_resizing(bool);
+
+struct resize_lock {
+	resize_lock();
+	~resize_lock();
+};
+
+void pump_events();
+
+#endif
